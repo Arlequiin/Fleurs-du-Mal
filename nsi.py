11067b628b4a50c57a15a49ef8c439a44c988292
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2022-11-27 01:02:52 +0100

    final float2iee754_64

--- nsi.py
+++ nsi.py
@@ -17,7 +17,19 @@ def fractionaltobin(n):
     return ''.join(l)
 def float2iee754_64(n):
   n=str(float(n)).split('.')
-  n[0]=str(intdectobin(int(n[0])))
+  p=0
+  if n[0].isdigit():
+    sign='+'
+    sign2='0'
+  else:
+    sign='-'
+    sign2='1'
+  n[0]=str(intdectobin(abs(int(n[0]))))
   n[1]=str(fractionaltobin(float('0.'+n[1])))
-  return '.'.join(n)
-print(float2iee754_64(10.2))
+  while len(n[0])!=1:
+    n[1]=n[0][-1]+n[1]
+    n[0]=n[0][:-1]
+    p+=1
+  return sign+'.'.join(n)+f" IEE({p})"
+
+print(float2iee754_64(float(input("Veuillez saisir un nombre, ce dernier sera converti en IEE754 (précision double)\n>>> "))))
